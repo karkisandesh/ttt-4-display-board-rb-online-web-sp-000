2f5af82f06a99cd635c41003785828d67228c963
Sandesh Karki <karkisandesh@hotmail.com>
2018-12-29 05:09:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -24,7 +24,7 @@ board = [" X  ", "  X ", " X ", "   ", " X ", "   ", "   ", "   ", "   "]
 
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]}  | #{board[2]}  "
+  puts " #{board[0]}| #{board[1]}| #{board[2]}  "
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
