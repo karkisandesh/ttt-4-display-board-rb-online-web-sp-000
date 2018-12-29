566a86b876847d19af7222212aeba2e3d49ce5e8
Sandesh Karki <karkisandesh@hotmail.com>
2018-12-29 05:40:35 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -162,8 +162,8 @@ describe "#display_board in 'lib/display_board.rb" do
      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
 
       # Don't touch the following lines.
-     # output = capture_puts{ display_board(board) } if defined?(display_board)
-     # rows = output.split("\n")
+     output = capture_puts{ display_board(board) } if defined?(display_board)
+    rows = output.split("\n")
 
       # Each line that starts with expect represents a row in the ouput.
       # The desired characters a row must include are provided by the String
