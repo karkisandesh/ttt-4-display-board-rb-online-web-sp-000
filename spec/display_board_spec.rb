42c9986319b0354ceab65df80d5b5b603447a4da
Sandesh Karki <karkisandesh@hotmail.com>
2018-12-29 05:44:00 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -218,6 +218,6 @@ describe "#display_board in 'lib/display_board.rb" do
       # Don't touch the following lines.
     # output = capture_puts{ display_board(board) } if defined?(display_board)
   #  rows = output.split("\n")
-  
+end
   end
 end
