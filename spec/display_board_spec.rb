afcd53766e343584087dad787896128c2632a113
Sandesh Karki <karkisandesh@hotmail.com>
2018-12-29 06:03:45 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -197,9 +197,9 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
 
-      expect(rows[0]).to eq("   |   |   ")
+       expect(rows[4]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   |   |   ")
+       expect(rows[4]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | O ")
     end
