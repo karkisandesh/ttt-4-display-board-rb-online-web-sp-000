0874def8a795655c0a8159b98442e7e98150099c
Sandesh Karki <karkisandesh@hotmail.com>
2018-12-29 05:08:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -20,11 +20,11 @@
   
 #end
 
-board = [" O  ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
+board = [" X  ", "  X ", " X ", "   ", " X ", "   ", "   ", "   ", "   "]
 
 
 def display_board(board)
-  puts " #{board[0]} |   |   "
+  puts " #{board[0]} | #{board[1]}  | #{board[2]}   "
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
