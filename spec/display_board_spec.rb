45803a2d71fe6380bffa11a01d64f244a3190bc4
Sandesh Karki <karkisandesh@hotmail.com>
2018-12-29 05:52:14 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -140,9 +140,9 @@ describe "#display_board in 'lib/display_board.rb" do
       
        board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
-      output = capture_puts{ display_board(board) } if defined?(display_board)
-    rows = output.split("\n")
-  end
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+  
       expect(rows[0]).to eq(" X | X | X ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" X | X | X ")
@@ -193,9 +193,8 @@ describe "#display_board in 'lib/display_board.rb" do
     
      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
-      output = capture_puts{ display_board(board) } if defined?(display_board)
-    rows = output.split("\n")
-  end
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
   
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
