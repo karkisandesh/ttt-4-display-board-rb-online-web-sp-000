4c7c0b562f5da35fed48371b3e032d1a485ce6a2
Sandesh Karki <karkisandesh@hotmail.com>
2018-12-29 05:48:35 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -142,7 +142,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       output = capture_puts{ display_board(board) } if defined?(display_board)
     rows = output.split("\n")
-  
+  end
       expect(rows[0]).to eq(" X | X | X ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" X | X | X ")
